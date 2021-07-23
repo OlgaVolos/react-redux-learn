3ee73bafeeaf40632c902806425fc03e17e1a079
OlgaVolos <72765623+OlgaVolos@users.noreply.github.com>
2021-07-23 22:57:08 +0300

    all counter in index

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import {useDispatch, useSelector} from "react-redux";
+
 
 function App() {
-  return (
-    <div>
+    // const store = useSelector((store)=> {
+    //   console.log(store);
+    //   return store
+    // })
+    const counter = useSelector(({counter}) => counter)
+    const dispatch = useDispatch()
 
-    </div>
-  );
+    return (
+        <div>
+            <h1>{counter}</h1>
+            <button onClick={()=> dispatch({type:  'INC'})}>INC</button>
+            <button onClick={()=> dispatch({type:  'DEC'})}>DEC</button>
+            <button onClick={()=> dispatch({type:  'RESET'})}>RESET</button>
+            <button onClick={()=> dispatch({type:  'INC_CUSTOM', payload: 10})}>INC_CUSTOM</button>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,62 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+
+let initialState = {
+    counter: 0,
+};
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'INC': {
+            return {
+                ...state,
+                counter: state.counter + 1
+            }
+        }
+        case 'INC_CUSTOM': {
+            return {
+                ...state,
+                counter: state.counter * action.payload
+            }
+        }
+        case 'DEC': {
+            return {
+                ...state,
+                counter: state.counter - 1
+            }
+        }
+        case 'RESET': {
+            return {
+                ...state,
+                counter: 0
+            }
+        }
+        default:
+            console.log('action', action.type, 'does not exist')
+            return state
+
+    }
+};
+const store = createStore(reducer)
+// console.log(store);
+// store.subscribe(()=> {
+//     console.log('tut', store.getState())
+// });
+//
+// store.dispatch({type: 'INC'})
+// store.dispatch({type: 'INC'})
+// store.dispatch({type: 'INC'})
+// store.dispatch({type: 'RESET'})
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
